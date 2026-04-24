85a42b5ee0155a30bc0bc130150b7cbd4d262d79
Invra <identificationsucks@gmail.com>
2026-04-24 14:34:22 +1000

    add: website testing block

---
+++ website/src/main.rs
@@ -0,0 +1,35 @@
+mod ui;
+// mod pages;
+
+use yew_router::prelude::*;
+use yew::prelude::*;
+
+#[derive(Clone, Routable, PartialEq)]
+pub enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! { <h1>{ "Home" }</h1> },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
---
+++ website/src/ui/mod.rs
@@ -0,0 +1,2 @@
+pub mod counter;
+pub use counter::Counter as CounterElem;
